7197992bb2f336c578f2096a8551fe6156640288
koniecdev <koniecdev@gmail.com>
2026-02-24 18:32:51 +0100

    DatVersionReader interface with response dto added with namespaces renames

---
+++ src/LotroKoniecDev.Application/Features/DatVersionReading/IDatVersionReader.cs
@@ -0,0 +1,8 @@
+﻿using LotroKoniecDev.Domain.Core.Monads;
+
+namespace LotroKoniecDev.Application.Features.DatVersionReading;
+
+public interface IDatVersionReader
+{
+    Result<DatVersionInfo> ReadVersion(string datFilePath);
+}

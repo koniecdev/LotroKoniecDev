51ea0f21529fe4f6359ff833552ec12a00b22307
koniecdev <koniecdev@gmail.com>
2026-02-24 19:18:15 +0100

    idatfileprotector and igamelauncher added

---
+++ src/LotroKoniecDev.Application/Abstractions/DatFilesServices/IDatFileProtector.cs
@@ -0,0 +1,10 @@
+﻿using LotroKoniecDev.Domain.Core.Monads;
+
+namespace LotroKoniecDev.Application.Abstractions.DatFilesServices;
+
+public interface IDatFileProtector
+{
+    Result Protect(string datFilePath);
+    Result Unprotect(string datFilePath);
+    bool IsProtected(string datFilePath);
+}

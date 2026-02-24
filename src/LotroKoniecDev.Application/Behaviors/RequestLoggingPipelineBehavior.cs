f3c1b0e72fa063cc5b4b2bd88d0a579e2541ec89
koniecdev <koniecdev@gmail.com>
2026-02-24 23:20:10 +0100

    pipeline behaviours added

---
+++ src/LotroKoniecDev.Application/Behaviors/RequestLoggingPipelineBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using LotroKoniecDev.Domain.Core.Monads;
+using Mediator;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+
+namespace LotroKoniecDev.Application.Behaviors;
+
+public sealed class RequestLoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IMessage
+    where TResponse : Result
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public RequestLoggingPipelineBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async ValueTask<TResponse> Handle(
+        TRequest message,
+        MessageHandlerDelegate<TRequest, TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        string requestName = typeof(TRequest).Name;
+        
+        _logger.LogInformation(
+            "Processing request {RequestName}",
+            requestName);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        
+        TResponse result = await next(message, cancellationToken);
+        
+        stopwatch.Stop();
+        
+        long duration = stopwatch.ElapsedMilliseconds;
+        
+        if (result.IsFailure)
+        {
+            using (LogContext.PushProperty("Error", result.Error, true))
+            {
+                _logger.LogError(
+                    "Completed request {RequestName} in {Duration}ms with error",
+                    requestName,
+                    duration);
+            }
+        }
+        
+        _logger.LogInformation(
+            "Completed request {RequestName} in {Duration}ms",
+            requestName, 
+            duration);
+        
+        return result;
+    }
+}
---
+++ src/LotroKoniecDev.Application/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Mediator;
+
+namespace LotroKoniecDev.Application.Behaviors;
+
+internal sealed class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IMessage
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipelineBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+    
+    public async ValueTask<TResponse> Handle(
+        TRequest message,
+        MessageHandlerDelegate<TRequest, TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            ValidationContext<TRequest> context = new(message);
+
+            ValidationResult[] validationFailures = await Task.WhenAll(
+                _validators.Select(validator => validator.ValidateAsync(context, cancellationToken)));
+
+            List<ValidationFailure> errors = validationFailures
+                .Where(validationResult => !validationResult.IsValid)
+                .SelectMany(validationResult => validationResult.Errors)
+                .ToList();
+        
+            if (errors.Count != 0)
+            {
+                throw new ValidationException(errors);
+            }
+        }
+
+        TResponse response = await next(message, cancellationToken);
+
+        return response;
+    }
+}

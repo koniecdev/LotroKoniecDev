95d5c314eac29fd9c542c2d988f0d9ae81b25b04
koniecdev <koniecdev@gmail.com>
2026-02-26 22:20:03 +0100

    serilog implemented in logging behavior

--- src/LotroKoniecDev.Application/Behaviors/RequestLoggingPipelineBehavior.cs
+++ src/LotroKoniecDev.Application/Behaviors/RequestLoggingPipelineBehavior.cs
@@ -2,56 +2,53 @@
 using LotroKoniecDev.Domain.Core.Monads;
 using Mediator;
 using Microsoft.Extensions.Logging;
-using Serilog.Context;
 
 namespace LotroKoniecDev.Application.Behaviors;
 
-public sealed class RequestLoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+public sealed class RequestLoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IMessage
     where TResponse : Result
 {
-    private readonly ILogger<TRequest> _logger;
+    private readonly ILogger<RequestLoggingPipelineBehavior<TRequest, TResponse>> _logger;
 
-    public RequestLoggingPipelineBehavior(ILogger<TRequest> logger)
+    public RequestLoggingPipelineBehavior(ILogger<RequestLoggingPipelineBehavior<TRequest, TResponse>> logger)
     {
         _logger = logger;
     }
-    
+
     public async ValueTask<TResponse> Handle(
         TRequest message,
         MessageHandlerDelegate<TRequest, TResponse> next,
         CancellationToken cancellationToken)
     {
         string requestName = typeof(TRequest).Name;
-        
+
         _logger.LogInformation(
             "Processing request {RequestName}",
             requestName);
 
         Stopwatch stopwatch = Stopwatch.StartNew();
-        
+
         TResponse result = await next(message, cancellationToken);
-        
+
         stopwatch.Stop();
-        
+
         long duration = stopwatch.ElapsedMilliseconds;
-        
+
         if (result.IsFailure)
         {
-            using (LogContext.PushProperty("Error", result.Error, true))
-            {
-                _logger.LogError(
-                    "Completed request {RequestName} in {Duration}ms with error",
-                    requestName,
-                    duration);
-            }
+            _logger.LogError(
+                "Completed request {RequestName} in {Duration}ms with error {@Error}",
+                requestName,
+                duration,
+                result.Error);
         }
-        
+
         _logger.LogInformation(
             "Completed request {RequestName} in {Duration}ms",
-            requestName, 
+            requestName,
             duration);
-        
+
         return result;
     }
 }
